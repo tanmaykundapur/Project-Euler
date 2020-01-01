5430cf8c1c4235ebbe0409684cbd8737365fae63
Tanmay Kundapur <52786904+tanmaykundapur@users.noreply.github.com>
2020-01-01 11:13:01 -0500

    Update Even Fibonacci Numbers.cpp

--- Problem #2/Even Fibonacci Numbers.cpp
+++ Problem #2/Even Fibonacci Numbers.cpp
@@ -5,24 +5,24 @@ using namespace std;
 int sum, total, n, t;
 
 int fib(int n1, int n2) {
-	if (sum <= n) {
-		sum = n1 + n2;
-		if (sum % 2 == 0) {
-			total += sum
-		}
-		return fib(n2, sum);
-	} else {
-		return 0;
-	}
+    sum = n1 + n2;
+    if (sum <= n) {
+        if (sum % 2 == 0) {
+            total += sum;   
+        }
+        return fib(n2, sum);
+    } else {
+        return 0;
+    }
 }
 
 int main() {
-	cin >> t;
-	for (int i = 0; i < t; i++) {
-		sum = 0, total = 0;
-		cin >> n;
-		fib(1, 2);
-		cout << total + 2 << "\n";
-	}
-	return 0;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        sum = 0, total = 0;
+        cin >> n;
+        fib(1, 2);
+        cout << total + 2 << "\n";
+    }
+    return 0;
 }
