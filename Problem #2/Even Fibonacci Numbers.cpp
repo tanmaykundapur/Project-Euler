f4f2085e22bfdf39e9f8f0627cc25f1ab27428a7
Tanmay Kundapur <52786904+tanmaykundapur@users.noreply.github.com>
2020-01-01 16:34:14 -0500

    Update Even Fibonacci Numbers.cpp

--- Problem #2/Even Fibonacci Numbers.cpp
+++ Problem #2/Even Fibonacci Numbers.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-int sum, total, n, t;
+unsigned long long sum, total, n, t;
 
-int fib(int n1, int n2) {
+int fib(unsigned long long n1, unsigned long long n2) {
     sum = n1 + n2;
-    if (sum <= n) {
+    if (sum < n) {
         if (sum % 2 == 0) {
-            total += sum;   
+            total += sum;    
         }
         return fib(n2, sum);
     } else {
