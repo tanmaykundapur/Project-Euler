dc7f3446ad222370d15ab6739588e355f0a76cf8
Tanmay Kundapur <52786904+tanmaykundapur@users.noreply.github.com>
2020-01-01 11:00:13 -0500

    Even_Fibonacci_Numbers.cpp

---
+++ Problem #2/Even Fibonacci Numbers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int sum, total, n, t;
+
+int fib(int n1, int n2) {
+	if (sum <= n) {
+		sum = n1 + n2;
+		if (sum % 2 == 0) {
+			total += sum
+		}
+		return fib(n2, sum);
+	} else {
+		return 0;
+	}
+}
+
+int main() {
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		sum = 0, total = 0;
+		cin >> n;
+		fib(1, 2);
+		cout << total + 2 << "\n";
+	}
+	return 0;
+}
